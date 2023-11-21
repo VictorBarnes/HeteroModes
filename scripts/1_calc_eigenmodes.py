c2cebe40b1419baf77c3d1de8c77afb9f37b1b05
VictorBarnes <victor.barnes365@gmail.com>
2023-11-21 15:30:04 +1100

    Initial commit to setup github repo

---
+++ scripts/1_calc_eigenmodes.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Calculate heterogeneous eigenmodes of a surface by solving the heterogeneous Helmholtz equation.
+"""
+
+import json
+import re
+import numpy as np
+import nibabel as nib
+import matplotlib.pyplot as plt
+from pathlib import Path
+from scipy import stats
+from scipy.sparse import diags
+from scipy.sparse.linalg import LinearOperator, eigs, eigsh
+from sklearn.preprocessing import MinMaxScaler
+from mpmath import mp
+from lapy import Solver, TriaMesh
+from lapy.diffgeo import compute_divergence, compute_gradient
+from brainspace.mesh import mesh_io, mesh_operations
+
+
+# Global variables
+DENSITIES = {"32k": 32492}
+CMEAN = 3352.4
+
+def calc_determinant(matrix):
+    mp.dps = 25
+    matrix_mp = [mp.mpf(float(elem)) for elem in matrix]
+    det = mp.mpf(1)
+    for elem in matrix_mp:
+        det *= elem
+
+    return det
+
+def scale_zscore(image, alpha=1.0, cmean=3352.4):
+    """Z-score and shift data by c_mean value (from Pang2023)"""
+    return cmean + alpha*stats.zscore(image)
+
+def scale_cmean(image, alpha=1.0, cmean=3352.4):
+    """Scale heterogeneity map to be a variation around the mean propagation speed (from Pang2023)
+    while preserving the mean (i.e. cmean is still the mean of the new distribution)."""
+    # Calculate normalized density (rho)
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    rho = scaler.fit_transform(image.reshape(-1, 1)).flatten()
+
+    return cmean + alpha * cmean * (rho - np.mean(rho))
+
+
+if __name__ == '__main__':
+    config_file = "/fs04/kg98/vbarnes/HeteroModes/scripts/config.json"
+    with open(config_file, encoding="UTF-8") as f:
+        config = json.load(f)
+    save_results = config["save_results"]
+    EMODE_DIR = config["emode_dir"]
+    SURF_DIR = config["surface_dir"]
+    RESULTS_DIR = config["results_dir"]
+    atlas = config["atlas"]
+    space = config["space"]
+    den = config["den"]
+    surf_type = config["surf"]
+    hemi = config["hemi"]
+    n_modes = config["n_modes"]
+    mask_medial = config["mask_medial"]
+    hetero_label = config["hetero_label"]
+    scale = config["scale"]
+    alpha_vals = np.arange(0.1, 1.1, 0.1)
+
+    # Load map chosen to paramaterize heterogeneity
+    if hetero_label is not None:
+        # Load heterogeneity map
+        mode_match = re.search(r"mode(\d+)", hetero_label)
+        if mode_match:
+            mode_id = int(mode_match.group(1)) - 1
+            hetero_modes = np.loadtxt(f"{EMODE_DIR}/hetero-None_atlas-{atlas}_space-{space}_"
+                                        f"den-{den}_surf-{surf_type}_hemi-{hemi}_n-{n_modes}_"
+                                        f"maskMed-{mask_medial}_emodes.txt")
+            hetero_map = hetero_modes[:, mode_id]
+        elif hetero_label in config["hetero_maps"]:
+            # Load heterogeneity map
+            hetero_file = config["hetero_maps"][hetero_label]
+            hetero_map = nib.load(hetero_file).agg_data()
+        else:
+            hetero_map = np.loadtxt(f"./data/hetero-{hetero_label}_surf-{surf_type}.txt")
+    else:
+        # No heterogeneity is encoded by an array of ones
+        hetero_map = np.ones(DENSITIES["32k"])
+        alpha_vals = [None]
+        scale = None
+
+    # Load surface template and medial wall mask
+    surf = mesh_io.read_surface(f"{SURF_DIR}/atlas-{atlas}_space-{space}_den-{den}_"
+                                f"surf-{surf_type}_hemi-{hemi}_surface.vtk")
+    medial = np.loadtxt(f"{SURF_DIR}/atlas-{atlas}_space-{space}_den-{den}_hemi-{hemi}_"
+                        f"medialMask.txt").astype(bool)
+    medial_inds = np.array([i for i, med in enumerate(medial) if med])
+
+    # Mask surface template and heterogeneous map
+    if mask_medial:
+        # Mask surface template
+        surf_masked = mesh_operations.mask_points(surf, medial)
+        v = surf_masked.Points
+        t = np.reshape(surf_masked.Polygons, [surf_masked.n_cells, 4])[:,1:4]
+        mesh = TriaMesh(v, t)
+        # Mask heterogeneous map
+        hetero_map = hetero_map[medial]
+    else:
+        # Initialise mesh without masking medial wall
+        mesh = TriaMesh.read_vtk(getattr(surf, hemi))
+
+    # Set up C matrix (only one column since alpha is None)
+    if hetero_label is None:
+        # Calculate C matrix
+        C = hetero_map * CMEAN
+        # Ensure C is doesn't have negative values
+        assert np.min(C) >= 0.0, f"Minimum value of C is negative: {np.min(C)}"
+        # Each term in C needs to be squared (according to the NFT equation)
+        C = (C ** 2).reshape(-1, 1)
+    else:
+        # Set up C matrix (each col defines the heterogeneity in propagation speed across the cortex)
+        C = np.zeros((len(hetero_map), len(alpha_vals)))
+        for i, alpha in enumerate(alpha_vals):
+            # Scale the heterogeneity map to vary around CMEAN (while still preserving CMEAN)
+            C[:, i] = scale_cmean(hetero_map, alpha=alpha, cmean=CMEAN)
+            # Ensure C doesn't have negative values
+            assert np.min(C[:, i]) >= 0.0, f"Minimum value of C is negative: {np.min(C[:, i])}"
+            # Each term in C needs to be squared (according to the NFT equation)
+            C[:, i] **= 2
+
+    # Solve heterogeneous Helmholtz equation (using each column of C as the heterogeneity)
+    C_reshaped = np.zeros((surf.n_points, C.shape[1]))
+    for i in range(C.shape[1]):
+        alpha = f"{alpha_vals[i]:.1f}" if isinstance(alpha_vals[i], float) else alpha_vals[i]
+        print(f"Atlas: {atlas} | Space: {space} | Density: {den} | Surface: {surf_type} | "
+            f"Hetero: {hetero_label} | Scaling: {scale} | alpha: {alpha} | cmean: {CMEAN}")
+
+        # Calculate the hetero value for each triangle by taking the average of the values at its vertices
+        C_tfunc = mesh.map_vfunc_to_tfunc(C[:, i])
+        # Initialise FEM solver and solve for eigenvalues and eigenmodes
+        fem = Solver(mesh, aniso=(0, 0), hetero=C_tfunc)
+        evals, emodes = fem.eigs(k=n_modes)
+
+        # Reshape emodes to match vertices of original surface
+        if mask_medial:
+            emodes_reshaped = np.zeros([surf.n_points, n_modes])
+            for mode in range(n_modes):
+                emodes_reshaped[medial_inds, mode] = emodes[:, mode]
+            # Reshape propagation speed map
+            C_reshaped[medial_inds, i] = C[:, i]
+
+        if save_results:
+            print("Saving eigenmodes and eigenvalues...")
+            # Set output file names and save
+            if hetero_label is None:
+                desc = f"hetero-{hetero_label}_atlas-{atlas}_space-{space}_den-{den}_surf-{surf_type}_" \
+                    f"hemi-{hemi}_n-{n_modes}_maskMed-{mask_medial}"
+            else:
+                desc = f"hetero-{hetero_label}_atlas-{atlas}_space-{space}_den-{den}_surf-{surf_type}_"\
+                    f"hemi-{hemi}_n-{n_modes}_scale-{scale}_alpha-{alpha}_maskMed-{mask_medial}"
+
+            evals_savefile = Path(EMODE_DIR, f"{desc}_evals.txt")
+            emodes_savefile = Path(EMODE_DIR, f"{desc}_emodes.txt")
+            cmap_savefile = Path(EMODE_DIR, "cmaps", f"{desc}_cmap.txt")
+            np.savetxt(evals_savefile, evals)
+            np.savetxt(emodes_savefile, emodes_reshaped)
+            np.savetxt(cmap_savefile, C_reshaped[:, i])
---
+++ scripts/3_recon_accuracy.m
@@ -0,0 +1,234 @@
+%% Code to understand what adding heterogeneity into eigenmodes actually does
+disp("running....")
+
+% Setup project functions
+projDir = '/fs04/kg98/vbarnes/HeteroModes';
+cd(fullfile(projDir, 'scripts'))
+% Load project functions using 'starProject.m' script
+startProject
+
+% Load config file
+config = jsondecode(fileread("/fs04/kg98/vbarnes/HeteroModes/scripts/config.json"));
+atlas = config.atlas;
+space = config.space;
+den = config.den;
+surf = config.surf;
+hemi = config.hemi;
+nModes = config.n_modes;
+realHeteroMaps = config.hetero_maps;
+emodeDir = config.emode_dir;
+surfDir = config.surface_dir;
+
+heteroLabel = "myelinmap"; % only plot one hetero map per figure
+scale = ["cmean"];
+alphaVals = 0.1:0.1:0.2;
+% alphaVals = ["0.1", "0.5", "1.0", "1.5", "2.0", "2.5"];
+dset = "hcp-tfMRI";
+
+% Set text for output fileneames
+heteroParams = {heteroLabel, scale};
+text = cell(1, numel(heteroParams));
+for ii = 1:numel(heteroParams)
+    if size(heteroParams{ii}, 2) > 1
+        text{ii} = strjoin(heteroParams{ii}, '-');
+    else
+        text{ii} = heteroParams{ii};
+    end
+end
+heteroText = text{1};
+scaleText = text{2};
+
+% TODO: set list of basis set filenames
+nBasisSets = length(alphaVals);
+basisSetLabels = alphaVals;
+
+disp("Loading modes and empirical data...")
+
+% Get cortex indices
+medialMask = dlmread(sprintf('%s/atlas-yeo_space-%s_den-%s_hemi-%s_medialMask.txt', surfDir, space, ...
+    den, hemi));
+cortexInds = find(medialMask);
+
+% Load geometric eigenmodes and eigenvalues
+geomDesc = 'hetero-%s_atlas-%s_space-%s_den-%s_surf-%s_hemi-%s_n-%i_maskMed-True';
+geomModes = dlmread(fullfile(emodeDir, sprintf(geomDesc, "None", atlas, space, den, surf, hemi, nModes) ...
+    + "_emodes.txt"));
+geomEvals = dlmread(fullfile(emodeDir, sprintf(geomDesc, "None", atlas, space, den, surf, hemi, nModes) ...
+    + "_evals.txt"));
+
+% Load heterogeneous eigenmodes and eigenvalues
+heteroDesc = 'hetero-%s_atlas-%s_space-%s_den-%s_surf-%s_hemi-%s_n-%i_scale-%s_alpha-%.1f_maskMed-True';
+heteroModes = zeros([size(geomModes), nBasisSets]);
+heteroEvals = zeros(nBasisSets, nModes);
+for ii=1:nBasisSets  
+    heteroModes(:, :, ii) = dlmread(fullfile(emodeDir, sprintf(heteroDesc, heteroLabel, atlas, ...
+        space, den, surf, hemi, nModes, scale, alphaVals(ii)) + "_emodes.txt")); 
+    heteroEvals(ii, :) = dlmread(fullfile(emodeDir, sprintf(heteroDesc, heteroLabel, atlas, space, ...
+        den, surf, hemi, nModes, scale, alphaVals(ii)) + "_evals.txt")); 
+end
+
+% Load empirical data to reconstruct
+if dset == "nm-images" || dset == "nm-subset"
+    % Load neuromaps empiricalData
+    empiricalData = load_images('/fs03/kg98/vbarnes/HBO_data/neuromaps-data/resampled/fsLR/dset-nm_desc-noPET_space-fsLR_den-32k_resampled.csv');
+    
+    if dset == "nm-subset"
+        all_empiricalData = empiricalData;
+        % Get subset of neuromaps data
+        subset = {'megalpha', 'fcgradient01', 'myelinmap', 'evoexp_xu2020',...
+            'scalinghcp'};
+        empiricalData = struct();
+        for ii = 1:numel(subset)
+            label = subset{ii};
+            empiricalData.(label) = all_empiricalData.(label);
+        end
+    end
+elseif dset == "hcp-tfMRI"
+    pang2023Dir = '/fs03/kg98/vbarnes/repos/BrainEigenmodes';
+    % Load hcp task fMRI data
+    empiricalData = load(sprintf('%s/data/empirical/S255_tfMRI_ALLTASKS_raw_lh.mat', pang2023Dir));
+    empiricalData = empiricalData.zstat;
+    % Only load first subject for each task contrast
+    contrasts = fieldnames(empiricalData);
+    for ii=1:numel(contrasts)
+        empiricalData.(contrasts{ii}) = empiricalData.(contrasts{ii})(:, 1);
+    end
+end
+empiricalLabels = fieldnames(empiricalData);
+nImages = numel(empiricalLabels);
+
+%% Calculate recon beta coefficients using 1 to num_modes eigenmodes
+tic
+disp("Computing beta coefficients...")
+geomReconBeta = zeros(nModes, nModes, nImages);
+heteroReconBeta = zeros(nModes, nModes, nImages);
+
+for ii = 1:length(alphaVals)
+    for jj = 1:nImages
+        image = empiricalData.(empiricalLabels{jj});
+        disp(empiricalLabels{jj})
+        
+        % If an image contains NaNs then set method to 'regression' to avoid errors
+        if any(isnan(image(cortexInds)))
+            method = 'regression';
+            disp('Found NaNs... Using regression')
+        else
+            method = 'matrix';
+        end
+        
+        for mode = 1:nModes
+            % Calculate beta spectra of empirical data using geometric modes
+            geomReconBeta(1:mode, mode, jj) = calc_eigendecomposition(image(cortexInds), ...
+                geomModes(cortexInds, 1:mode), method);
+            % Calculate beta spectra of empirical data using hetero modes
+            heteroReconBeta(1:mode, mode, jj) = calc_eigendecomposition(image(cortexInds), ...
+                heteroModes(cortexInds, 1:mode, ii), method);
+        end
+    end
+    fprintf("Finished computing beta coefficients: %.2f mins\n", toc/60)
+
+    %% Calculate reconstruction accuracy using 1 to num_modes eigenmodes
+    tic
+    disp("Computing reconstruction accuracy...")
+    geomReconCorrs = zeros(nImages, nModes);   
+    heteroReconCorrs = zeros(nImages, nModes);
+    for jj = 1:nImages
+        image = empiricalData.(empiricalLabels{jj});
+        for mode = 1:nModes
+            reconTemp = geomModes(cortexInds, 1:mode)*geomReconBeta(1:mode, mode, jj);
+            geomReconCorrs(jj, mode) = corr(image(cortexInds), reconTemp, "rows", "complete");
+
+            reconTemp = heteroModes(cortexInds, 1:mode)*heteroReconBeta(1:mode, mode, jj);
+            heteroReconCorrs(jj, mode) = corr(image(cortexInds), reconTemp, "rows", "complete");
+        end
+    end
+    fprintf("Finished computing reconsruction corrs: %.2f mins\n", toc/60)
+
+    % Change labels to plotting labels for clearer figures
+    if dset == "nm_images" || dset == "nm_subset"
+        config_file = fileread('/fs03/kg98/vbarnes/human-brain-observatory/scripts/configurations/configHBO.json');
+        configHBO = jsondecode(config_file);
+        plotting_labels = configHBO.neuromaps.plotting_labels;
+        for jj = 1:length(empiricalLabels)
+            original_label = empiricalLabels{jj};
+            empiricalLabels{jj} = plotting_labels.(original_label);
+        end
+    end
+
+    fprintf("Finished computing reconstruction difference: %.2f mins\n", toc/60)
+
+    %% Compare reconstruction accuracy and plot
+    fig = figure('Position', [200 200 1000 600]);
+
+    recon_corr_combined = cat(3, heteroReconCorrs - geomReconCorrs);
+
+    % Set clims
+    clims_combined = zeros(2,2);
+    for kk=1:1
+        recon_corr = recon_corr_combined(:, :, kk);
+        clim_max = max(recon_corr(:,2:end),[],'all');
+        clim_min = min(recon_corr(:,2:end),[],'all');
+        clims_combined(kk,:) = [clim_min, clim_max];
+    end
+
+    tl = tiledlayout(1, 1);
+    for kk=1:1   
+        data_to_plot = recon_corr_combined(:,:,kk);
+        %     clim = clims_combined(kk,:);
+        % Make absolute colorbar for all two eigenmodes
+        clim = [min(clims_combined(:,1)), max(clims_combined(:,2))]; 
+        % Make colorbar symmetric according to the maximum absolute value
+        clim = max(abs(min(clims_combined(:,1))), abs(max(clims_combined(:,2))))*[-1 1]; 
+        
+        nexttile();
+        imagesc(data_to_plot)
+        for jj=1:nImages-1
+            yline(jj+0.5, 'k-');
+        end
+
+        caxis(clim)
+        colormap(bluewhitered)
+        % cbar = colorbar;
+        set(gca, 'fontsize', 10, 'ticklength', [0.02, 0.02], 'xlim', [1, nModes], 'ytick', 1:nImages,...
+            'yticklabel', empiricalLabels, 'ticklabelinterpreter', 'none')
+        if kk~=1
+            set(gca, 'yticklabel', {})
+        end
+        xlabel(tl, 'number of modes')
+        title({'Heterogeneous modes'; sprintf('(%s | alpha: %.1f)', heteroLabel, alphaVals)}, 'fontweight', 'normal', 'fontsize', 18)
+    end
+
+    cbar = colorbar('Location', 'eastoutside');
+    ylabel(cbar, 'reconstruction accuracy difference', 'fontsize', 16)
+
+    % Save image
+    savecf(sprintf("%s/results/evaluation/hetero-%s_surf-%s_scale-%s_alpha-%.1f_empiricalData-%s_reconAccuracyDiff", ...
+        projDir, heteroText, surf, scaleText, alphaVals(ii), dset), ".png", 200)
+end
+
+% %% Plot reconstruction acuracy paths
+% figure; 
+
+% % Hetero modes recon accuracy
+% subplot(1, 2, 1); 
+% hold on; 
+% for i=1:n_images
+%     plot(recon_corr_hetero(i, 2:n_modes)); 
+% end
+% hold off; 
+% xlabel("number of modes"); title("Heterogeneous"); pbaspect([1 1 1]);
+% ylabel("reconstruction accuracy");
+% ylim([0, 1])
+% legend(labels, "location", "southeast", 'Interpreter', 'none')
+
+% % Geometric modes recon accuracy
+% subplot(1, 2, 2); 
+% hold on; 
+% for i=1:n_images
+%     plot(recon_corr_geom(i, 2:n_modes)); 
+% end
+% hold off; 
+% xlabel("number of modes"); title("Geometric"); pbaspect([1 1 1]);
+% ylabel("reconstruction accuracy");
+% ylim([0, 1])
+% legend(labels, "location", "southeast", 'Interpreter', 'none')
