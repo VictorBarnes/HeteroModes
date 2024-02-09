a3a33a0bed107b9b063b984683ad71fcb7ef552d
VictorBarnes <victor.barnes365@gmail.com>
2024-02-09 21:50:16 +1100

    Update calc_eigenmodes.sh to submit jobs as a job array

--- scripts/slurm/calc_eigenmodes.sh
+++ scripts/slurm/calc_eigenmodes.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
-
-#SBATCH --job-name=calcModes
-#SBATCH --output=/fs03/kg98/vbarnes/slurm/calc_modes_%j.out
-#SBATCH --mail-user=victor.barnes@monash.edu
-# SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --account=kg98
-#SBATCH --time=2-00:00:00
-#SBATCH --cpus-per-task=20
+#SBATCH --job-name=calcEMs
+#SBATCH --output=/fs03/kg98/vbarnes/slurm/calc_modes_%j.out
+#SBATCH --time=1:00:00
+#SBATCH --ntasks=1
 #SBATCH --mem-per-cpu=4G
+#SBATCH --mail-user=victor.barnes@monash.edu
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=END
+#SBATCH --array=1-2
+
+echo -e "${SLURM_ARRAY_TASK_ID}"
+
+alpha_beta_file="/fs04/kg98/vbarnes/HeteroModes/data/alpha_beta_all.csv"
+# Skip the header row and select the row based on the SLURM_ARRAY_TASK_ID
+row=$(tail -n +2 "${alpha_beta_file}" | awk -F',' "NR==${SLURM_ARRAY_TASK_ID} {print}")
+alpha=$(echo "${row}" | awk -F',' '{print $1}')
+beta=$(echo "${row}" | awk -F',' '{print $2}')
 
 echo "Activating virtual environment"
 source /fs03/kg98/vbarnes/miniconda/bin/activate
 conda activate HeteroModes
 
-cd /fs04/kg98/vbarnes/HeteroModes/scripts
+cd /fs04/kg98/vbarnes/HeteroModes
 
 echo "===== Begin running script ====="
-python 1_calc_eigenmodes.py
+python scripts/1_calc_eigenmodes.py ${config_file} ${alpha} ${beta}
