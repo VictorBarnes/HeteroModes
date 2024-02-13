543e3b2b55ed08ed73b1151ca6719f3d76caeb11
Victor Barnes <victor.barnes365@gmail.com>
2024-02-13 14:19:25 +1100

    Change alpha values to include negatives.

--- scripts/gen_ab_combs.py
+++ scripts/gen_ab_combs.py
@@ -3,8 +3,11 @@ import numpy as np
 import itertools
 
 # Define the values for the pairs
-alpha = np.arange(0.2, 3.2, 0.2)
-beta = np.sort(np.append(np.arange(-10.0, 11.0, 1.0), [-0.5, 0.5]))
+alpha = np.sort(np.concatenate((np.arange(0.2, 3.2, 0.2), 
+                                np.arange(0.2, 3.2, 0.2) * -1)))
+beta = np.sort(np.concatenate((np.arange(1.0, 11.0, 1.0), 
+                               np.arange(1.0, 11.0, 1.0) * -1, 
+                               [-0.5, 0.5])))
 
 # Generate all possible pairs
 combs = list(itertools.product(alpha, beta))
@@ -12,4 +15,4 @@ combs = [(round(a, 1), round(b, 1)) for a, b in combs]
 
 # Create a DataFrame with the pairs and save
 combs_df = pd.DataFrame(combs, columns=['alpha', 'beta'])
-combs_df.to_csv("data/alpha-beta-combs_all.csv", index=None)
+combs_df.to_csv("data/csParamCombs_all.csv", index=None)
