e940af29c53c2af3b28a8844bc80d8857e8bd84f
Victor Barnes <victor.barnes365@gmail.com>
2024-09-26 16:01:03 +1000

    Untrack matlab files

--- scripts/sandbox/setupProject.m
+++
@@ -1,9 +0,0 @@
-%% Starts project
-% different superDirs for windows and linux
-% ignored by git
-clc;
-
-projDir = "/fs04/kg98/vbarnes/HeteroModes";
-
-reposDir = "/fs03/kg98/vbarnes/repos";
-run(fullfile(projDir, 'scripts', 'startProject.m'));
