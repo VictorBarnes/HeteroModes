ec50086475d3d335e38a18305ac11f828b7292fc
Victor Barnes <victor.barnes365@gmail.com>
2025-01-23 11:33:35 +1100

    Generate medmask based on empirical bold data

--- scripts/model_rs/gen_ext_input.py
+++ scripts/model_rs/gen_ext_input.py
@@ -1,8 +1,9 @@
 import os
+import h5py
 import numpy as np
 import nibabel as nib
 from dotenv import load_dotenv
-from neuromaps.datasets import fetch_fslr
+
 
 load_dotenv()
 PROJ_DIR = os.getenv("PROJ_DIR")
@@ -10,7 +11,9 @@ PROJ_DIR = os.getenv("PROJ_DIR")
 den = "4k"
 parc_name = None
 if parc_name is None:
-    medmask = nib.load(fetch_fslr(den)["medial"][0]).darrays[0].data.astype(bool)
+    bold_data = h5py.File(f"{PROJ_DIR}/data/empirical/HCP_unrelated-445_rfMRI_hemi-L_nsubj-384_parc-None_fsLR{den}_hemi-L_BOLD.hdf5", 'r')
+    bold_emp = bold_data['bold'][:]
+    medmask = np.where(bold_emp[:, 0, 0] != 0, True, False)
 else:
     parc_file = f"{PROJ_DIR}/data/parcellations/parc-{parc_name}_space-fsLR_den-{den}_hemi-L.label.gii"
     parc = nib.load(parc_file).darrays[0].data.astype(int)
