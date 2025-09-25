30e5bbaf1058cf39456c714c6936f98296e2645c
VictorBarnes <victor.barnes365@gmail.com>
2025-09-25 16:45:56 +1000

    Change synapticden to sv2a

--- scripts/model_rest/generate_nulls.py
+++ scripts/model_rest/generate_nulls.py
@@ -1,24 +1,17 @@
 # %%
 import os
+import json
 import numpy as np
 import nibabel as nib
 from neuromaps.datasets import fetch_atlas
 from eigenstrapping import SurfaceEigenstrapping
 from heteromodes.utils import load_hmap, get_project_root
 
-
 PROJ_DIR = get_project_root()
 
-hetero_labels = {
-    "myelinmap": "T1w/T2w",
-    "thickness": "Cortical thickness",
-    "synapticden": "Synaptic density",
-    "odi": "ODI",
-    "ndi": "NDI",
-    "genel4PC1": "Layer IV",
-    "eiratio1.2": "E:I ratio",
-    "megtimescale": "MEG timescale"
-}
+with open(f"{PROJ_DIR}/data/heteromaps/human/heteromap_labels.json", "r") as f:
+    config = json.load(f)
+heteromap_labels = config["heteromap_labels"]
 resample = True
 n_nuls = 1000
 n_modes = 500
@@ -28,8 +21,8 @@ fslr = fetch_atlas("fsLR", den)
 surff = str(fslr["midthickness"][0])
 medmask = nib.load(f"{PROJ_DIR}/data/empirical/human/space-fsLR_den-4k_hemi-L_desc-nomedialwall.func.gii").darrays[0].data.astype(bool)
 
-for hmap_label in hetero_labels.keys():
-    print(f"Processing {hetero_labels[hmap_label]}...")
+for hmap_label in heteromap_labels.keys():
+    print(f"Processing {heteromap_labels[hmap_label]}...")
     hmap = load_hmap(hmap_label, species="human", trg_den=den)
     eigen = SurfaceEigenstrapping(surface=surff, data=hmap, medial=medmask, seed=365, num_modes=n_modes, 
                                 resample=resample)
