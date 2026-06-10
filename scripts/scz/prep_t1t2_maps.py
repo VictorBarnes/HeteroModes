[MNT] Fix incorrect masking in hcp-ep t1t2 maps

scripts/scz/prep_t1t2_maps.py
@@ -12,11 +12,11 @@ PROJ_DIR = get_project_root()
 data_dir = "/fs03/kg98/vbarnes/hcp-ep/"
 
 medmask = nib.load(
-    f"{PROJ_DIR}/data/empirical/human/space-fsLR_den-4k_hemi-L_desc-nomedialwall.func.gii"
+    f"{PROJ_DIR}/data/empirical/human/hcp-ep_space-fsLR_den-4k_hemi-L_desc-nomedialwall.func.gii"
 ).darrays[0].data.astype(bool)
 
-hc_ids = np.loadtxt(PROJ_DIR / "data" / "hcp-ep" / "ids_hc_run-1.txt").astype(int)
-scz_ids = np.loadtxt(PROJ_DIR / "data" / "hcp-ep" / "ids_scz_run-1.txt").astype(int)
+hc_ids = np.loadtxt(PROJ_DIR / "data" / "empirical" / "human" / "hcp-ep-hc-run-1-2_nsubj-45_desc-subjects.txt").astype(int)
+scz_ids = np.loadtxt(PROJ_DIR / "data" / "empirical" / "human" / "hcp-ep-scz-run-1-2_nsubj-74_desc-subjects.txt").astype(int)
 
 print(len(hc_ids), len(scz_ids))
 
@@ -38,13 +38,17 @@ for i, cohort in enumerate(["hc", "scz"]):
     avg_map = np.mean(all_maps, axis=0)
     gii = construct_shape_gii(data=unmask(avg_map, medmask))
     nib.save(
-        gii, PROJ_DIR / "data" / "hcp-ep" / f"cohort-{cohort}_avg-myelinmap.gii"
+        gii, PROJ_DIR / f"data/heteromaps/human/source-hcpep_desc-{cohort}-myelinmap_space-fsLR_den-4k_hemi-L.func.gii"
     )
 
 #%% load computed maps and plot
 
-t1t2_hc = nib.load(PROJ_DIR / "data" / "hcp-ep" / "cohort-hc_avg-myelinmap.gii").darrays[0].data
-t1t2_scz = nib.load(PROJ_DIR / "data" / "hcp-ep" / "cohort-scz_avg-myelinmap.gii").darrays[0].data
+t1t2_hc = nib.load(
+    PROJ_DIR / "data/heteromaps/human/source-hcpep_desc-hc-myelinmap_space-fsLR_den-4k_hemi-L.func.gii"
+).darrays[0].data
+t1t2_scz = nib.load(
+    PROJ_DIR / "data/heteromaps/human/source-hcpep_desc-scz-myelinmap_space-fsLR_den-4k_hemi-L.func.gii"
+).darrays[0].data
 diff = t1t2_scz - t1t2_hc
 
 maps = np.stack([t1t2_hc, t1t2_scz], axis=0)
@@ -104,6 +108,3 @@ plot_surf(
     clim=(vmin, vmax),
 )
 plt.show()
-
-
-